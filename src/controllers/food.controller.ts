0f27efb68a9e58a2ebd4ede8fad129c1683527e8
Ankit Sharma <generalankit51@gmail.com>
2026-02-02 20:01:39 +0545

    Added food controller

--- src/controllers/food.controller.ts
+++ src/controllers/food.controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import { FoodModel } from "../models/Food.model";
 
-// Get all foods
 export const getAllFoods = async (req: Request, res: Response) => {
     try {
         const foods = await FoodModel.find({ isAvailable: true })
